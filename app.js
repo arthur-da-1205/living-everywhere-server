04941f395cc296ee0660e88c05f41c59a4e8e95a
arthur-da-1205 <arthur.darwanto@gmail.com>
2022-02-24 16:27:08 +0700

    Add View Item and View Booking in Admin Dashboard

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
+const mongoose = require("mongoose");
+
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 const adminRouter = require("./routes/admin");
