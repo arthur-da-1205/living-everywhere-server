45910f169f96d121fff168ed79b6c7a7513e3bd6
arthur-da-1205 <arthur.darwanto@gmail.com>
2022-02-28 15:04:31 +0700

    CRUD Bank - Delete Bank Data

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -136,6 +136,20 @@ module.exports = {
       res.redirect("/admin/bank");
     }
   },
+  deleteBank: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const bank = await Bank.findOne({ _id: id });
+      await bank.remove();
+      req.flash("alertMessage", "Success Delete Bank");
+      req.flash("alertStatus", "success");
+      res.redirect("/admin/bank");
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/admin/bank");
+    }
+  },
 
   viewItem: (req, res) => {
     res.render("admin/item/view_item", {
