e61a8ae04c47743285fd063a3cfc38e9ecdfaa3a
arthur-da-1205 <arthur.darwanto@gmail.com>
2022-02-28 15:00:39 +0700

    CRUD Bank - Update Bank Data

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,7 @@
 const Category = require("../models/Category");
 const Bank = require("../models/Bank");
+const fs = require("fs-extra");
+const path = require("path");
 
 module.exports = {
   viewDashboard: (req, res) => {
@@ -98,6 +100,39 @@ module.exports = {
       req.flash("alertStatus", "success");
       res.redirect("/admin/bank");
     } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", "success");
+      res.redirect("/admin/bank");
+    }
+  },
+  editBank: async (req, res) => {
+    try {
+      const { id, bankName, accountNumber, name } = req.body;
+      const bank = await Bank.findOne({ _id: id });
+      if (req.file == undefined) {
+        bank.bankName = bankName;
+        bank.accountNumber = accountNumber;
+        bank.name = name;
+        await bank.save();
+
+        req.flash("alertMessage", "Success Add Bank");
+        req.flash("alertStatus", "success");
+        res.redirect("/admin/bank");
+      } else {
+        await fs.unlink(path.join(`public/${bank.imageUrl}`));
+        bank.bankName = bankName;
+        bank.accountNumber = accountNumber;
+        bank.name = name;
+        bank.imageUrl = `images/${req.file.filename}`;
+        await bank.save();
+
+        req.flash("alertMessage", "Success Add Bank");
+        req.flash("alertStatus", "success");
+        res.redirect("/admin/bank");
+      }
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", "danger");
       res.redirect("/admin/bank");
     }
   },
