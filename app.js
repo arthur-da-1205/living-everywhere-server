6086b2e3a6166147a5e5c25651bd06a76f587ac2
arthur-da-1205 <arthur.darwanto@gmail.com>
2022-02-28 11:03:31 +0700

    Adding Status Message

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ mongoose.connect("mongodb://localhost:27017/myapp");
 
 const methodOverride = require("method-override");
 
+const session = require("express-session");
+const flash = require("connect-flash");
+
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 const adminRouter = require("./routes/admin");
@@ -20,6 +23,16 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(methodOverride("_method"));
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 },
+  })
+);
+app.use(flash());
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
